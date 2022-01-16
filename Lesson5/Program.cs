12a2099d527cefa41c19e8340ef59f9ddfdae0b2
Spawn95 <disk-9@mail.ru>
2022-01-16 16:05:14 +0700

    part3

--- Lesson5/Program.cs
+++ Lesson5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Lesson5
 {
@@ -7,10 +8,10 @@ namespace Lesson5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Программа записала текущее время в файл проекта «startup.txt».");                 
-            string programName = "startup.txt";
-            string userTime = Convert.ToString(DateTime.Now);
-            File.WriteAllText(programName, userTime);
+            Console.WriteLine("Введите произвольный набор чисел от 0 до 255, для записи в бинарный файл.");                 
+            string userNumbers = Console.ReadLine();          
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(new FileStream("UserNumbers.bin", FileMode.OpenOrCreate), userNumbers); 
         }
     }
 }
